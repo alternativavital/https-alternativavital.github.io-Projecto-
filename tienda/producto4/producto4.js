99df38c35891e2b22c105b0d5da01dac8bebc56b
alternativavital <160396630+alternativavital@users.noreply.github.com>
2024-02-22 19:21:13 +0100

    22/2/24

---
+++ tienda/producto4/producto4.js
@@ -0,0 +1,11 @@
+const total = document.querySelector("#total"),
+	  botones1 = document.querySelector("#botones1");
+
+botones1.addEventListener("click", e => {
+	btn = e.target.firstChild.textContent;
+	/coje el valor del boton para multiplicarlo/ 
+	resultado = btn * 15;
+	console.log(resultado);
+	total.innerText= resultado; 
+})
+
